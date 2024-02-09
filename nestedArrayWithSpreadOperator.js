f3191f188babd97e2d63a84de1b648d50764657f
Mousumi Saha <mousumisaha927@gmail.com>
2024-02-08 23:40:15 -0700

    alt way

--- nestedArrayWithSpreadOperator.js
+++ nestedArrayWithSpreadOperator.js
@@ -18,3 +18,13 @@ const nestedArray = [1, [2, [3, 4]], 5, [6]];
 const flattenedArray = flattenArray(nestedArray);
 
 console.log(flattenedArray);
+
+//alt way
+function flattenArray(arr) {
+  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
+}
+
+const nestedArray = [1, [2, [3, 4]], 5, [6]];
+const flattenedArray = flattenArray(nestedArray);
+
+console.log(flattenedArray);
